7d08d5774486456b8cb9bfab956c34a4fe1db414
VasoBechvaia <supersuperadze1@gmail.com>
2022-09-30 12:42:52 +0400

    first commit

---
+++ day3/homework3.py
@@ -0,0 +1,9 @@
+my_name = input("enter your name")
+my_surname = input("enter your surname")
+my_age = input("enter your age")
+my_academy = input("enter your academy")
+my_hobbie = input ("enter your hobby")
+
+
+my_sentence = "my name is {0}, my surname is {1}, my age is {2}, my academy is {3}, my hobbie is {4}. va" .format(my_name, my_surname, my_age, my_academy, my_hobbie)
+print(my_sentence)
---
+++ day3/lesson3.py
@@ -0,0 +1,21 @@
+# my_name = "vaso"
+# my_surname = "bechvaia"
+# my_age = 16
+
+
+
+# my_sentence = "my name is {0} my surname is {1} my age is {2}" .format(my_name, my_age, my_surname)
+
+# print(my_sentence)
+
+# if "q" in my_name:
+#     print("sheicavs q-s")
+# else:
+#     print("arsheicavs q-s")
+
+#input
+#output
+
+my_name = input("enter your name")
+
+print("chemisaxelia" + my_name) 
